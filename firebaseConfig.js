60bbac4715e3db79f78cb71f2df6c68650bd92fd
Kevin Natera <NateraKevin1@gmail.com>
2025-10-28 19:31:36 -0400

    update Firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration using Vite's env variables
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDHN2p7FVMARburqBfphQm_jdh2CWqz35o",
+  authDomain: "freelancefinance-2ec74.firebaseapp.com",
+  projectId: "freelancefinance-2ec74",
+  storageBucket: "freelancefinance-2ec74.firebasestorage.app",
+  messagingSenderId: "810916509823",
+  appId: "1:810916509823:web:5e495d14cfb5db4fed674a",
 };
 
 // Initialize Firebase
